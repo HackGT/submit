52003ea37cdeb19ca4e3e19095c3ff892a4fe8c1
Ayush Goyal <agoyal2001@gmail.com>
2020-09-24 00:46:22 -0400

    Add ant design and initial frontend layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './util/PrivateRoute';
+import { HeartOutlined } from "@ant-design/icons";
+import { Layout, Spin } from 'antd';
+import Dashboard from "./components/dashboard/Dashboard";
+import Navigation from './components/navigation/Navigation';
+import AdminHome from './components/admin/AdminHome';
+import CreateSubmission from "./components/submission/CreateSubmission";
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("/auth/check")
+      .then((res) => {
+        setUser(res.data);
+        setLoading(false);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [setUser]);
+
+  if (loading) {
+    return <Spin style={{ position: "absolute", top: "48%", left: "48%" }} />;
+  }
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout className="App" style={{ minHeight: "100vh" }}>
+        <Header style={{ padding: "0px 30px" }}>
+          <Navigation user={user} />
+        </Header>
+        <Content id="wrapper">
+          <div style={{ background: "#fff", padding: "24px", flexGrow: 1 }}>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/create" component={CreateSubmission} />
+              <PrivateRoute exact path="/admin" component={AdminHome} user={user} />
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          {"Made with "}
+          <HeartOutlined />
+          {" by the HackGT Team"}
+        </Footer>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ client/src/components/submission/CreateSubmission.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateSubmission: React.FC = () => {
+  return <div>Create Submission</div>;
+}
+
+export default CreateSubmission;
