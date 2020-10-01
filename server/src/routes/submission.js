6a6baff9d14900f58289e381dce1817ba10630f7
Rahul Rajan <rahul.rajanus@gmail.com>
2020-09-30 23:52:40 -0400

    stage 1 backend

--- server/src/routes/submission.js
+++ server/src/routes/submission.js
@@ -1,5 +1,6 @@
+let {Team, User} = require('../schema')
 let submissionRoutes = express.Router();
-
+const GRAPHQLURL = process.env.GRAPHQLURL || 'https://registration.hack.gt/graphql'
 /*
     Form Step 1: Retrieve emails of users on team
     - Query emails from check-in and ensure users accepted to event
@@ -9,8 +10,58 @@ let submissionRoutes = express.Router();
     Response:
     - errors
 */
-submissionRoutes.route("/team-validation").post((req, res) => {
+submissionRoutes.route("/team-validation").post(async (req, res) => {
+    const emails = req.body.emails
+    emails.forEach(email => {
+        let confirmed = false;
+        let name = "";
+        const query = `
+        query($search: String!) {
+            search_user(search: $search, offset: 0, n: 1) {
+                users {
+                    name
+                    confirmed
+                }
+            }
+        }`;
+        const variables = {
+            search: email
+        };
+        const options = { method: 'POST',
+            url: GRAPHQLURL,
+            headers:
+            {
+                Authorization: 'Bearer ' + process.env.GRAPHQLAUTH,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                query,
+                variables
+            })
+        };
 
+        await request(options, (err, res, body) => {
+            if (err) { return console.log(err); }
+            if (JSON.parse(body).data.search_user.users.length > 0) {
+                confirmed = JSON.parse(body).data.search_user.users[0].confirmed;
+                name = JSON.parse(body).data.search_user.users[0].name;
+            }
+        });
+        if(!confirmed) {
+            res.send({"error": true, "message": "User: " + email + " not confirmed for HackGT 7"});
+        }
+        User.count({"email": email}, (err, count) {
+            if(err) {
+                console.log(err)
+                throw new Error(err);
+            }
+            if(count == 0) {
+                let new_user = new User({
+                    name: "Team " + user1._id + '' + user2._id,
+                })
+            }
+        })
+    });
 });
 
 /*
