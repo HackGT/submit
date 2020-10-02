302e0e360e65b3385fc8f7a98522af1b8e0394a1
Rahul Rajan <rahul.rajanus@gmail.com>
2020-10-02 04:10:59 -0400

    fix not confirmed glitch

--- server/src/routes/submission.js
+++ server/src/routes/submission.js
@@ -1,6 +1,7 @@
 let {Team, User, Submission} = require('../schema')
 const express = require("express");
 const request = require("request");
+const axios = require("axios")
 
 let submissionRoutes = express.Router();
 const GRAPHQLURL = process.env.GRAPHQLURL || 'https://registration.2020.hack.gt/graphql'
@@ -19,7 +20,10 @@ submissionRoutes.route("/team-validation").post(async (req, res) => {
         res.send({"error": true, "message": "Invalid body: email does not match"})
         return;
     }
-
+    if(emails.length > 4) {
+        res.send({"error": true, "message": "Too many members on team"})
+        return;
+    }
     let submission = null;
     if(req.body.submissionId) {
         submission = await Submission.findById(req.body.submissionId);
@@ -32,6 +36,7 @@ submissionRoutes.route("/team-validation").post(async (req, res) => {
     }
     let errConfirmed = null;
     const userIds = await Promise.all(emails.map(async email => {
+        console.log("hello",email)
         const query = `
         query($search: String!) {
             search_user(search: $search, offset: 0, n: 1) {
@@ -51,28 +56,22 @@ submissionRoutes.route("/team-validation").post(async (req, res) => {
                 Authorization: 'Bearer ' + process.env.GRAPHQLAUTH,
                 'Content-Type': "application/json"
             },
-            body: JSON.stringify({
+            data: JSON.stringify({
                 query,
                 variables
             })
         };
-
-        await request(options, (err, res, body) => {
-            if (err) { console.log(err); }
-            console.log(
-                JSON.parse(body).data.search_user.users[0]
-            )
-            let confirmed = false;
-            const users = JSON.parse(body).data.search_user.users
-            if (users.length > 0) {
-                confirmed = users[0].confirmed;
-            }
-            if(!confirmed) {
-                errConfirmed = email;
-                return;
-            }
-        })
-
+        let resp = await axios(options);
+        let confirmed = false;
+        const users = resp.data.data.search_user.users
+        if (users.length > 0) {
+            confirmed = users[0].confirmed;
+        }
+        if(!confirmed) {
+            console.log("nah",email)
+            errConfirmed = email;
+            return;
+        }
         let updatedUser = await User.findOneAndUpdate({"email": email}, {
             "$addToSet": {submissions: submission._id}
         },{
