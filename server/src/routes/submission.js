3f11f0f35c8f37bb1dd39e07c5c5c12833f14ad5
Ayush Goyal <agoyal2001@gmail.com>
2020-10-10 22:06:02 -0400

    Update submission.js file readability

--- server/src/routes/submission.js
+++ server/src/routes/submission.js
@@ -1,43 +1,31 @@
-
-const { Team, User, Submission } = require("../schema");
+const { User, Submission } = require("../schema");
 const { config } = require("../common");
 const express = require("express");
-const request = require("request");
-const axios = require("axios")
-var rp = require("request-promise")
-var soup = require('jssoup').default;
-const cheerio = require('cheerio')
+const axios = require("axios");
+const rp = require("request-promise");
+const cheerio = require('cheerio');
+
+const GRAPHQL_URL = process.env.GRAPHQL_URL || 'https://registration.2020.hack.gt/graphql';
+const CURRENT_HACKATHON = "HackGT 7";
+const HACKGT_DEVPOST = "https://hackgt2020.devpost.com/";
 
 let submissionRoutes = express.Router();
-const GRAPHQLURL = process.env.GRAPHQLURL || 'https://registration.2020.hack.gt/graphql';
-const CURRENT_HACKATHON = "HackGT 7";
-const HACKGT_DEVPOST = "https://hackgt2020.devpost.com/"
 
 
 /*
-    Form Step 1: Retrieve emails of users on team
     - Query emails from check-in and ensure users accepted to event
     - Create new user objects for users not in db (with email field and name from check-in)
-    - update database with team info
-
-    Response:
-    - errors
 */
-submissionRoutes.route("/team-validation").post(async (req, res) => {
-    const resp = await validateTeam(req.body.members, req.user.email)
-    return res.send(resp);
-});
-
 validateTeam = async (members, user_email) => {
     if (!members || members.length === 0) {
-        return res.send({"error": true, "message": "Must include at least one member"});
+        return res.send({ error: true, message: "Must include at least one member" });
     } else if (members.length > 4) {
-        return res.send({"error": true, "message": "Too many members on team"})
+        return res.send({ error: true, message: "Too many members on team" })
     }
 
     const emails = members.map(member => member.email);
     if (emails[0] !== user_email) {
-        return {"error": true, "message": "Email does not match current user"}
+        return { error: true, message: "Email does not match current user" }
     }
 
     let errConfirmed = null;
@@ -63,7 +51,7 @@ validateTeam = async (members, user_email) => {
 
         const options = {
             method: 'POST',
-            url: GRAPHQLURL,
+            url: GRAPHQL_URL,
             headers: {
                 "Authorization": 'Bearer ' + process.env.GRAPHQL_AUTH,
                 "Content-Type": "application/json"
@@ -105,58 +93,69 @@ validateTeam = async (members, user_email) => {
     }))
 
     if (errConfirmed) {
-        return {"error": true, "message": "User: " + errConfirmed + " not confirmed for " + CURRENT_HACKATHON};
+        return { error: true, message: "User: " + errConfirmed + " not confirmed for " + CURRENT_HACKATHON };
     }
     if (errSubmission) {
-        return {"error": true, "message": "User: " + errSubmission + " already has a submission for " + CURRENT_HACKATHON};
+        return {
+            error: true,
+            message: "User: " + errSubmission + " already has a submission for " + CURRENT_HACKATHON
+        };
     }
 
     const eligiblePrizes = getEligiblePrizes(registrationUsers);
 
-    console.log(eligiblePrizes);
-
     return { error: false, eligiblePrizes };
-
 }
+
+/*
+    - Ensure url is the right devpost url
+    - Ensure project isn't submitted to multiple hackathons
+*/
 validateDevpost = async (devpost_url) => {
-    if(!devpost_url) {
-        return {"error": true, "message": "No url specified"}
+    if (!devpost_url) {
+        return { error: true, message: "No url specified" };
     }
-    const hostname = new URL(devpost_url).hostname
-    if(hostname != "devpost.com") {
-        return {"error": true, "message": "Invalid URL: Not devpost domain"}
+
+    const hostname = new URL(devpost_url).hostname;
+    if (hostname !== "devpost.com") {
+        return { error: true, message: "Invalid URL: Not devpost domain" };
     }
+
     let html = ""
     try {
         html = await rp(devpost_url);
-    } catch(err) {
-        return {"error": true, "message": "Invalid Project URL"}
+    } catch (err) {
+        return { error: true, message: "Invalid Project URL" };
     }
 
-    const $ = cheerio.load(html)
-    devpost_urls = []
-    var submitted = false
+    const $ = cheerio.load(html);
+    let devpost_urls = [];
+    let submitted = false;
+
     $('#submissions').find('ul').children("li").each((index, elem) => {
-        const item = $(elem).find("div a").attr("href")
-        if(item) {
-            devpost_urls.push(item)
-            if(item == HACKGT_DEVPOST) {
+        const item = $(elem).find("div a").attr("href");
+        if (item) {
+            devpost_urls.push(item);
+            if (item === HACKGT_DEVPOST) {
                 submitted = true;
             }
         }
-    })
-    var eligible = submitted && (devpost_urls.length == 1)
-    if(eligible) {
-        return {"error": false}
-    }
-    else if(!submitted) {
-        return {"error": true, "message": "Project not submitted to HackGT 7 Devpost"};
-    }
-    else if(!(devpost_urls.length > 1)) {
-        return {"error": true, "message": "Project submitted to multiple hackathons"};
+    });
+
+    let eligible = submitted && (devpost_urls.length === 1);
+    if (eligible) {
+        return { error: false };
+    } else if (!submitted) {
+        return { error: true, message: "Project not submitted to HackGT 7 Devpost" };
+    } else if (devpost_urls.length !== 1) {
+        return { error: true, message: "Project submitted to multiple hackathons" };
     }
 }
 
+/*
+    - Classify team into category based on user tracks (from registration)
+    - Return eligible prizes based on team type
+*/
 getEligiblePrizes = (users) => {
     switch (CURRENT_HACKATHON) {
         case "HackGT 7":
@@ -165,7 +164,10 @@ getEligiblePrizes = (users) => {
 
             users.forEach(user => {
                 if (!user.confirmationBranch) {
-                    return res.send({"error": true, "message": "User: " + user.email + " does not have a confirmation branch"});
+                    return res.send({
+                        error: true,
+                        message: "User: " + user.email + " does not have a confirmation branch"
+                    });
                 }
 
                 if (user.confirmationBranch === "Emerging Participant Confirmation") {
@@ -183,65 +185,52 @@ getEligiblePrizes = (users) => {
     }
 }
 
-/*
-    Form Step 2:
-    - Get prize categories for create from
-    - Classify team into category based on user tracks (from registration)
-    - Validate prizes selected and make sure team is eligible
-    - update database with prize categories
+submissionRoutes.route("/team-validation").post(async (req, res) => {
+    const resp = await validateTeam(req.body.members, req.user.email)
+    res.send(resp);
+});
 
-    Response:
-    - errors
-*/
+// TODO: Fill in prize validation as needed
 submissionRoutes.route("/prize-validation").post((req, res) => {
-    res.send({});
+    res.send({ error: false });
 });
 
-
-
-/*
-    Form Step 3:
-    - Get devpost url
-    - Make sure devpost submission prize categories matches up with submitted prize categories
-    - Run duplicate check
-
-    Response:
-    - errors
-*/
 submissionRoutes.route("/devpost-validation").post(async (req, res) => {
-    const devpost_url = req.body.devpost;
-    const resp = await validateDevpost(devpost_url)
-    return res.send(resp)
+    const resp = await validateDevpost(req.body.devpost);
+    res.send(resp);
 });
-
-
 
 // Last step of the form, all the data is passed in here and a submission should be created
 submissionRoutes.route("/create").post(async (req, res) => {
-    if(!req.body.submission) {
-        return res.send({error: true, messsag: "Invalid submission"})
+    if (!req.body.submission) {
+        return res.send({ error: true, message: "Invalid submission" });
     }
     const data = req.body.submission;
+
     const teamValidation = await validateTeam(data.members, req.user.email)
-    if(teamValidation.error) {
-        return res.send({error: true, messsage: "Invalid team"})
+    if (teamValidation.error) {
+        return res.send({ error: true, message: "Invalid team" })
     }
+
     const devpostValidation = await validateDevpost(data.devpost)
-    if(devpostValidation.error) {
-        return res.send({error: true, messsage: "Invalid devpost submission"})
+    if (devpostValidation.error) {
+        return res.send({ error: true, message: "Invalid devpost submission" })
     }
-    let submission = await Submission.create({
-        hackathon: CURRENT_HACKATHON,
-        devpost: data.devpost,
-        name: data.name,
-        members: await User.find({email: data.members.map(member => member.email)}),
-        categories: data.prizes
-    });
 
-    console.log(req.body);
-    res.send("Done");
+    try {
+        await Submission.create({
+            hackathon: CURRENT_HACKATHON,
+            devpost: data.devpost,
+            name: data.name,
+            members: await User.find({ email: data.members.map(member => member.email) }),
+            categories: data.prizes
+        });
+    } catch (err) {
+        return res.send({ error: true, message: "Submission could not be saved - please contact help desk" });
+    }
+
+    res.send({ error: false });
 });
-
 
 submissionRoutes.route("/submission/:submissionId").get(async (req, res) => {
     try {
@@ -268,7 +257,6 @@ submissionRoutes.route("/dashboard").get(async (req, res) => {
         console.error(err);
         return res.send({ error: true, message: err.message });
     }
-})
-
+});
 
 exports.submissionRoutes = submissionRoutes;
